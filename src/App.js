c24785b71d92374735c3ab746ff134e904e7409b
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-09-09 15:39:19 +0200

    init project structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import "./normalize.css"
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
