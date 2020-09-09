e274aad932906df9fa08f37c9cbb905a3eee5b9e
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-09-09 16:28:45 +0200

    setup router

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 import "./normalize.css"
 import "./styles.css"
+import {Switch, Route} from "react-router-dom"
 
 function App() {    
     return (
         <div>
             <Header />
+            <Switch>
+                <Route exact path="/">
+                    <Photos />
+                </Route>
+
+                <Route path ="/cart">
+                    <Cart />
+                </Route>
+            </Switch>
             <h1>Home Page</h1>
         </div>
     )
