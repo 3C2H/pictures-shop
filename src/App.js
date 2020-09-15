92f959bc3ae72b1a3cac8832f580d30fd5cdb449
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-09-15 15:55:21 +0200

    changed style imports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
-import "./normalize.css"
-import "./styles.css"
 import {Switch, Route} from "react-router-dom"
 
 function App() {    
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter as Router} from "react-router-dom"
 import App from "./App"
+import "./normalize.css"
+import "./styles.css"
 
 ReactDOM.render(
   <Router>
