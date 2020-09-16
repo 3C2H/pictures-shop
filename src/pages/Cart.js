94ecf5c98931a734a650c8b9a5e834c23fe143eb
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-09-16 15:09:57 +0200

    cart page functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React, {useContext} from "react"
+import CartItem from "../components/CartItem"
+
+import {Context} from "../Context"
 
 function Cart() {
+    const {cartItems} = useContext(Context)
+    const cartItemElements = cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
+    ))
+    
     return (
         <main className="cart-page">
             <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total: </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>    
         </main>
     )
 }
