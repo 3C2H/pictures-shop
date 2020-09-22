1a33b4977d1ca202896a807334701fd8101e279a
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-09-22 17:03:20 +0200

    cost display (fixed price)

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,6 +5,9 @@ import {Context} from "../Context"
 
 function Cart() {
     const {cartItems} = useContext(Context)
+const totalCost = 5.99 * cartItems.length
+const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
+
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
@@ -13,7 +16,7 @@ function Cart() {
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalCostDisplay} </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>    
